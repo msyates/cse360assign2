a45e6f34509956ae45e4c756084bcc3b5eb742f6
myate <myate@myates-laptop>
2019-10-05 12:22:57 -0700

    Initial Commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,70 @@
+/**
+ * By Mark Yates
+ * ASUID 1207697747
+ * Test Seat L-A-10
+ * Assignment 2
+ *
+ * The file containing the AddingMachine Class, which works as a visual calculator
+ * by taking input, storing and processing it, and displaying the output along
+ * with the transaction history
+ */
+
+package cse360assign2;
+
+
+public class AddingMachine {
+	
+	private int total;
+	
+	/**
+	 * Class Constructor
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**
+	 * Class Getter for getTotal
+	 * 
+	 * @return the int stored in getTotal
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Method to add a given value to the current total
+	 * 
+	 * @param value user input to be added to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Method to subtract a given value from the current total
+	 * 
+	 * @param value user input to be subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Method to display history of inputs
+	 * 
+	 * @return string of user inputs
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Method to clear the current use
+	 */
+	public void clear() {
+		
+	}
+	
+}
---
+++ cse360assign2/src/cse360assign2/AddingMachineTest.java
@@ -0,0 +1,23 @@
+/**
+ * By Mark Yates
+ * ASUID 1207697747
+ * Test Seat L-A-10
+ * Assignment 2
+ *
+ * The test file for the AddingMachine Class
+ */
+
+package cse360assign2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddingMachineTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
